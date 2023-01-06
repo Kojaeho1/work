eccd222fded0efc66d65dcafef66d7746f462003
Kojaeho1 <rhwogh0310@gmail.com>
2023-01-06 14:31:45 +0900

    workproject

---
+++ src/sample01/MainClass.java
@@ -0,0 +1,53 @@
+package sample01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello multicampus");
+		
+		System.out.println("hello");
+		
+		System.out.print("hi");
+		
+		System.out.print("월드");
+		// 기능 : escape sequence
+		//       \n(개행) 줄바꿈  \b(backspace) \"(따옴표 출력), \t(띄어쓰기), \\
+		System.out.print("World\n");
+		
+		System.out.println("헬로\b우");
+		
+		System.out.println("\"hello world\"");
+		
+		System.out.println("\thello \tworld");
+		System.out.println("\t월드\t헬로");
+		
+		System.out.println("\\헬로우 월드\\");
+		
+		System.out.printf("--%s--\n", "hello");
+		//f -> format
+		
+		System.out.println(1024);
+		
+		System.out.println("hello" + "World");
+	}
+	
+	// 한줄 주석문 -> 컴파일 x
+
+	/*
+	  범위 주석문
+	  다른 언어 주석문들
+	  html <!-- -->
+	  
+	  jsp <%-- --%>
+	  
+	  mysql --
+	  oracle --
+	  
+	  
+	  python # ''' 주석문 '''
+	 */
+	
+}
+
+
